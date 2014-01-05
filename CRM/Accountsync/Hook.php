f31ee572aaaec93926ef8ff9f99ecbadaa7c36ad
Eileen <eileen@fuzion.co.nz>
2014-01-05 22:26:00 +1300

    Add hook class

---
+++ CRM/Accountsync/Hook.php
@@ -0,0 +1,21 @@
+<?php
+
+class CRM_Accountsync_Hook {
+
+  /**
+   * This hook allows an entity retrieved from Xero to be altered prior to processing
+   * It is somewhat like the pre hook except that
+   * - it allows the save to be cancelled
+   * - it deals with a single Accounts entity not a CiviCRM entity
+   *
+   * @param string $entity entity - eg. 'contact'
+   * @param array $data data from accounts being processing
+   * @param boolean $save save? - set this to false if it should be skipped
+   */
+  static function accountPullPreSave($entity, &$data, &$save) {
+    return CRM_Utils_Hook::singleton()->invoke(3, $entity,
+      $data, $save, CRM_Core_DAO::$_nullObject, CRM_Core_DAO::$_nullObject,
+      'civicrm_accountPullPreSave'
+    );
+  }
+}
