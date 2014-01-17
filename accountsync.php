e85dd47e899138e0c8a31d82c26c943517f63e33
Eileen McNaughton <eileen@fuzion.co.nz>
2014-01-18 00:34:46 +1300

    Add post-hook support for creating invoices

--- accountsync.php
+++ accountsync.php
@@ -88,19 +88,29 @@ function accountsync_civicrm_post($op, $objectName, $objectId, &$objectRef){
   $entities = civicrm_api3('setting', 'get', array('group' => 'Account Sync'));
   $createEntities = $entities['values'][CRM_Core_Config::domainID()]['account_sync_queue_contacts'];
   $updateEntities = $entities['values'][CRM_Core_Config::domainID()]['account_sync_queue_update_contacts'];
+  $invoiceEntities = $entities['values'][CRM_Core_Config::domainID()]['account_sync_queue_create_invoice'];
 
-  if(!in_array($objectName, array_merge($createEntities, $updateEntities))) {
-    return;
+  if(in_array($objectName, array_merge($createEntities, $updateEntities))) {
+    _accountsync_create_account_contact($objectRef->contact_id, in_array($objectName, $createEntities));
   }
-  $contactID = $objectRef->contact_id;
-  $accountContact = array('contact_id' => $contactID, 'accounts_needs_update' => 1);
+  if(in_array($objectName, $invoiceEntities)) {
+    // we won't do updates as the invoices get 'locked' in the accounts system
+    _accountsync_create_account_invoice($objectRef->id, FALSE);
+  }
+}
 
+/**
+ * Create account contact record or set needs_update flag
+ * @param integer $contactID
+ */
+function _accountsync_create_account_contact($contactID, $createNew) {
+  $accountContact = array('contact_id' => $contactID, 'accounts_needs_update' => 1);
   try {
     $accountContact['id'] = civicrm_api3('account_contact', 'getvalue', array('plugin' => 'xero', 'return' => 'id', 'contact_id' => $contactID));
   }
   catch (CiviCRM_API3_Exception $e) {
     // new contact
-    if(!in_array($objectName, $createEntities)) {
+    if(!$createNew) {
       return;
     }
   }
@@ -111,5 +121,47 @@ function accountsync_civicrm_post($op, $objectName, $objectId, &$objectRef){
   catch (CiviCRM_API3_Exception $e) {
     // unknown failure
   }
-
 }
+  /**
+   * Create account invoice record or set needs_update flag
+   * @param integer $contributionID
+   */
+  function _accountsync_create_account_invoice($contributionID, $createNew) {
+    $accountInvoice = array('contribution_id' => $contributionID, 'accounts_needs_update' => 1);
+    try {
+      $accountInvoice['id'] = civicrm_api3('account_invoice', 'getvalue', array('plugin' => 'xero', 'return' => 'id', 'contribution_id' => $contributionID));
+    }
+    catch (CiviCRM_API3_Exception $e) {
+      // new contact
+      if(!$createNew) {
+        return;
+      }
+    }
+    $accountInvoice['plugin'] = 'xero';
+    try {
+      civicrm_api3('account_invoice', 'create', $accountInvoice);
+    }
+    catch (CiviCRM_API3_Exception $e) {
+      // unknown failure
+    }
+  }
+
+  /**
+   * Sample code from webform_civicrm.module 7.x-1.4
+   * Implements hook_civicrm_merge().
+   * Update submission data to reflect new cid when contacts are merged.
+   */
+  function accountsync_civicrm_merge($type, $data, $new_id = NULL, $old_id = NULL, $tables = NULL) {
+    if (!empty($new_id) && !empty($old_id) && $type == 'sqls') {
+      try {
+        //@todo - this will only move old contact ref to the new one - if both have xero accounts
+        // then it will fail
+        $accountContact = civicrm_api3('account_contact', 'getsingle', array('plugin' => 'xero', 'contact_id' => $old_id));
+        $accountContact['contact_id'] = $new_id;
+        $accountContact = civicrm_api3('account_contact', 'create', $accountContact);
+      }
+      catch (Exception $e) {
+        //nothing to do here
+      }
+    }
+  }
--- settings/AccountSync.setting.php
+++ settings/AccountSync.setting.php
@@ -12,6 +12,7 @@ return array(
     'description' => 'Entity updates to trigger queuing for contact creation',
     'title' =>  'Entities to trigger contact sync',
     'help_text' => 'When these entities are created the contact will be queued for sync',
+    'default' => array('Contribution'),
     'html_type' => 'advmultiselect',
     'quick_form_type' => 'Element',
     'html_attributes' => array(
@@ -32,6 +33,7 @@ return array(
     'title' =>  'Entities to trigger contact update',
     'help_text' => 'When these entities are created the contact will be queued for update',
     'html_type' => 'advmultiselect',
+    'default' => array('Contact', 'Email', 'Address', 'Phone'),
     'quick_form_type' => 'Element',
     'html_attributes' => array(
       'Contact' => 'Contact',
@@ -40,6 +42,24 @@ return array(
       'Address' => 'Address',
       'Email' => 'Email',
       'Phone' => 'Phone',
+    )
+  ),
+  'account_sync_queue_create_invoice' => array(
+    'group_name' => 'Account Sync',
+    'group' => 'accountsync',
+    'name' => 'account_sync_queue_create_invoice',
+    'type' => 'Array',
+    'add' => '4.4',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => 'Trigger Invoice Creation when entity created',
+    'title' =>  'Entities to trigger invoice create',
+    'help_text' => 'When these entities are created an invoice will be queued for create',
+    'html_type' => 'advmultiselect',
+    'default' => array('Contribution'),
+    'quick_form_type' => 'Element',
+    'html_attributes' => array(
+      'Contribution' => 'Contribution',
     )
   ),
  );
