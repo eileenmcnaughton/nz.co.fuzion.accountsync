7affd8b621659bc58176ccc0bddd9a67c1bba095
Eileen McNaughton <eileen@fuzion.co.nz>
2015-03-13 13:32:40 +1300

    tid-yups towards phpcs standard

--- api/v3/AccountInvoice.php
+++ api/v3/AccountInvoice.php
@@ -1,11 +1,12 @@
 <?php
 
 /**
- * AccountInvoice.create API specification (optional)
+ * AccountInvoice.create API specification.
+ *
  * This is used for documentation and validation.
  *
  * @param array $spec description of fields supported by this API call
- * @return void
+ *
  * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
  */
 function _civicrm_api3_account_invoice_create_spec(&$spec) {
@@ -13,10 +14,12 @@ function _civicrm_api3_account_invoice_create_spec(&$spec) {
 }
 
 /**
- * AccountInvoice.create API
+ * AccountInvoice.create API.
  *
  * @param array $params
- * @return array API result descriptor
+ *
+ * @return array
+ *   API result descriptor
  * @throws API_Exception
  */
 function civicrm_api3_account_invoice_create($params) {
@@ -24,10 +27,13 @@ function civicrm_api3_account_invoice_create($params) {
 }
 
 /**
- * AccountInvoice.delete API
+ * AccountInvoice.delete API.
  *
  * @param array $params
- * @return array API result descriptor
+ *
+ * @return array
+ *   API result descriptor
+ *
  * @throws API_Exception
  */
 function civicrm_api3_account_invoice_delete($params) {
@@ -35,10 +41,13 @@ function civicrm_api3_account_invoice_delete($params) {
 }
 
 /**
- * AccountInvoice.get API
+ * AccountInvoice.get API.
  *
  * @param array $params
- * @return array API result descriptor
+ *
+ * @return array
+ *   API result descriptor
+ *
  * @throws API_Exception
  */
 function civicrm_api3_account_invoice_get($params) {
@@ -46,18 +55,22 @@ function civicrm_api3_account_invoice_get($params) {
 }
 
 /**
- * AccountInvoice.get derived invoice
+ * AccountInvoice.get derived invoice.
+ *
  * This function compiles what I think an invoice returned from the CiviCRM api should look like
  * - unfortunately it takes a bit of building!
- * NB I didn't got down the invoice.get path because it seemed to be that in
+ * NB I did not go down the invoice.get path because it seemed to be that in
  * future it might be used for something else
  *
  * We also have a problem doing getfields for this as the whole getfields _spec thing struggles a bit here
  * - the generic getfields needs to be over-ridden due to the DAO not being in core
  * & then the spec thing doesn't quite work *
  * @todo - write a getfields that works - but for now array('id' => $contributionID)
+ *
  * @param array $params
- * @return array API result descriptor
+ *
+ * @return array
+ *   API result descriptor
  * @throws API_Exception
  */
 function civicrm_api3_account_invoice_getderived($params) {
@@ -65,74 +78,78 @@ function civicrm_api3_account_invoice_getderived($params) {
 }
 
 /**
- * AccountContact.getfields API
+ * AccountContact.getfields API.
+ *
  * (we can't rely on generic as it won't look at our DAO)
  *
  * @param array $params
- * @return array API result descriptor
+ *
+ * @return array
+ *   API result descriptor
+ *
  * @throws API_Exception
  */
 function civicrm_api3_account_invoice_getfields($params) {
   return civicrm_api3_create_success(
-    array (
-      'id' => array (
+    array(
+      'id' => array(
         'name' => 'id',
         'type' => 1,
         'required' => 1,
-        'api.aliases' => array (
+        'api.aliases' => array(
           '0' => 'account_invoice_id'
         )
       ),
 
-      'contribution_id' => array (
+      'contribution_id' => array(
         'name' => 'contribution_id',
         'type' => 1,
         'FKClassName' => 'CRM_Contribute_DAO_Contribution'
       ),
 
-      'accounts_invoice_id' => array (
+      'accounts_invoice_id' => array(
         'name' => 'accounts_invoice_id',
         'type' => 2,
         'maxlength' => 128,
         'size' => 20
       ),
 
-      'last_sync_date' => array (
+      'last_sync_date' => array(
         'name' => 'last_sync_date',
         'type' => 256,
         'title' => 'Last Sync Date',
         'default' => 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
       ),
 
-      'accounts_modified_date' => array (
+      'accounts_modified_date' => array(
         'name' => 'accounts_modified_date',
         'type' => 4,
         'title' => 'Accounts Modified Date'
       ),
-      'accounts_status_id' => array (
+      'accounts_status_id' => array(
         'name' => 'accounts_status_id',
         'type' => 2,
         'title' => 'Accounts Status',
         'maxlength' => 32,
       ),
-      'accounts_data' => array (
+      'accounts_data' => array(
         'name' => 'accounts_data',
         'type' => 32,
         'title' => 'Account System Data',
-        'size' => 45
+        'size' => 45,
       ),
-      'error_data' => array (
+      'error_data' => array(
         'name' => 'error_data',
         'type' => 32,
         'title' => 'Error Data',
-        'size' => 45
+        'size' => 45,
       ),
-      'accounts_needs_update' => array (
+      'accounts_needs_update' => array(
         'name' => 'accounts_needs_update',
         'type' => CRM_Utils_Type::T_BOOLEAN,
         'title' => 'Update Accounts?',
       ),
-      'plugin' => array (
+      'plugin' => array(
         'name' => 'plugin',
         'type' => 2,
         'title' => 'Account Plugin',
@@ -143,11 +160,12 @@ function civicrm_api3_account_invoice_getfields($params) {
     ));
 }
 /**
- * AccountInvoice.create API specification (optional)
+ * AccountInvoice.create API specification (optional).
+ *
  * This is used for documentation and validation.
  *
  * @param array $spec description of fields supported by this API call
- * @return void
+ *
  * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
  */
 function _civicrm_api3_account_invoice_update_contribution(&$spec) {
@@ -155,18 +173,21 @@ function _civicrm_api3_account_invoice_update_contribution(&$spec) {
 }
 
 /**
- * AccountInvoice.create API
+ * AccountInvoice.create API.
  *
  * @param array $params
- * @return array API result descriptor
+ *
+ * @return array
+ *   API result descriptor
+ *
  * @throws API_Exception
  */
 function civicrm_api3_account_invoice_update_contribution($params) {
-  if($params['accounts_status_id'] == 1) {
+  if ($params['accounts_status_id'] == 1) {
     CRM_Accountsync_BAO_AccountInvoice::completeContributionFromAccountsStatus($params);
     return civicrm_api3_create_success();
   }
-  if($params['accounts_status_id'] == 3) {
+  if ($params['accounts_status_id'] == 3) {
     CRM_Accountsync_BAO_AccountInvoice::cancelContributionFromAccountsStatus($params);
     return civicrm_api3_create_success();
   }
