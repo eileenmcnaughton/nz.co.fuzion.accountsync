fd1cccb16a369d7600c90a7b3a48e5e849023c7f
Ken West <ken.west@internode.on.net>
2015-04-20 21:57:50 +1000

    Add in function to map financial types to account codes. Temporary fix.

--- CRM/Accountsync/BAO/AccountInvoice.php
+++ CRM/Accountsync/BAO/AccountInvoice.php
@@ -203,3 +203,17 @@ class CRM_Accountsync_BAO_AccountInvoice extends CRM_Accountsync_DAO_AccountInvo
   }
 
 }
+
+/**
+ * Get the accounting code from the financial type id.
+ *
+ * @param int $financialTypeID
+ * @return mixed
+ */
+function tapestrymultiaccounts_get_civicrm_account_code($financialTypeID) {
+  static $codes = array();
+  if (!in_array($financialTypeID, $codes)) {
+    $codes[$financialTypeID] = CRM_Financial_BAO_FinancialAccount::getAccountingCode($financialTypeID);
+  }
+  return $codes[$financialTypeID];
+}
