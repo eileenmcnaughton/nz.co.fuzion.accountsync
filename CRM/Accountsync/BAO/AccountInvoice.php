c397fadd3c0e568022a35522b5f07408fe1bb514
Ken West <ken.west@internode.on.net>
2015-04-21 15:02:56 +1000

    Unprotect cancel and complete methods

--- CRM/Accountsync/BAO/AccountInvoice.php
+++ CRM/Accountsync/BAO/AccountInvoice.php
@@ -145,7 +145,7 @@ class CRM_Accountsync_BAO_AccountInvoice extends CRM_Accountsync_DAO_AccountInvo
   /**
    * Update contributions in civicrm based on their status in Xero.
    */
-  protected static function completeContributionFromAccountsStatus() {
+  static function completeContributionFromAccountsStatus() {
     $sql = "
       SELECT contribution_id
       FROM civicrm_account_invoice cas
@@ -164,7 +164,7 @@ class CRM_Accountsync_BAO_AccountInvoice extends CRM_Accountsync_DAO_AccountInvo
    *
    * @todo - I don't believe this will adequately cancel related entities
    */
-  protected static function cancelContributionFromAccountsStatus($params) {
+  static function cancelContributionFromAccountsStatus($params) {
     //get pending registrations
     $sql = "SELECT  cas.contribution_id
       FROM civicrm_account_invoice cas
