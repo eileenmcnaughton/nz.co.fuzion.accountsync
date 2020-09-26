bec372569374ec53f8a7a1c3c7a66faaca132b79
Ken West <west.ken.au@gmail.com>
2020-09-26 15:41:28 +1000

    With CiviXero, when I click 'Queue Sync to Xero' an entry is created in the Accounts Contact table with connector_id NULL. The Contact Push job ignores this. Set connector_id to zero if none supplied.

--- CRM/Accountsync/BAO/AccountContact.php
+++ CRM/Accountsync/BAO/AccountContact.php
@@ -13,6 +13,7 @@ class CRM_Accountsync_BAO_AccountContact extends CRM_Accountsync_DAO_AccountCont
     $className = 'CRM_Accountsync_DAO_AccountContact';
     $entityName = 'AccountContact';
     $hook = empty($params['id']) ? 'create' : 'edit';
+    $params['connector_id'] = $params['connector_id'] ?? 0;
 
     CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
     $instance = new $className();
