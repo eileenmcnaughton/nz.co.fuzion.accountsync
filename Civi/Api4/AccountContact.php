959583d47585bb68a52711aa18502ba8cc201df9
Eileen McNaughton <emcnaughton@wikimedia.org>
2021-07-24 18:39:02 +1200

    Add v4 api

---
+++ Civi/Api4/AccountContact.php
@@ -0,0 +1,15 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * AccountContact entity.
+ *
+ * Provided by the Account Sync extension.
+ *
+ * @search secondary
+ *
+ * @package Civi\Api4
+ */
+class AccountContact extends Generic\DAOEntity {
+
+}
---
+++ Civi/Api4/AccountInvoice.php
@@ -0,0 +1,15 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * AccountInvoice entity.
+ *
+ * Provided by the Account Sync extension.
+ *
+ * @search secondary
+ *
+ * @package Civi\Api4
+ */
+class AccountInvoice extends Generic\DAOEntity {
+
+}
