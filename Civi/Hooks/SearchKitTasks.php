eba881b8de40c7208059f2a5cdd160f76bea786f
Matthew Wire <mjw@mjwconsult.co.uk>
2022-12-29 13:06:13 +0000

    Use new SearchKitTasks display param to restrict search tasks to specific displays

--- Civi/Hooks/SearchKitTasks.php
+++ Civi/Hooks/SearchKitTasks.php
@@ -14,29 +14,42 @@ class SearchKitTasks {
    * @param array $tasks
    * @param bool $checkPermissions
    * @param int|null $userID
+   * @param array $search
+   * @param array $display
    */
-  public function run(array &$tasks, bool $checkPermissions, ?int $userID): void {
-    foreach (['AccountContact', 'AccountInvoice'] as $entity) {
-      $tasks[$entity]['queue'] = [
-        'title' => E::ts('Re-queue %1', [1 => 'record']),
-        'apiBatch' => [
-          'action' => 'update',
-          'params' => ['values' => ['accounts_needs_update' => TRUE, 'error_data' => '']],
-          'runMsg' => E::ts('Queueing synchronization ...'),
-          'successMsg' => E::ts('Successfully re-queued %1 record.'),
-          'errorMsg' => E::ts('An error occurred while attempting to queue the record.'),
-        ],
-      ];
-      $tasks[$entity]['dismiss'] = [
-        'title' => E::ts('Dismiss error %1', [1 => 'record']),
-        'apiBatch' => [
-          'action' => 'update',
-          'params' => ['values' => ['is_error_resolved' => TRUE]],
-          'runMsg' => E::ts('Dismissing error ...'),
-          'successMsg' => E::ts('Successfully dismissed error for %1 record.'),
-          'errorMsg' => E::ts('An error occurred while attempting to dismiss the error.'),
-        ],
-      ];
+  public function run(array &$tasks, bool $checkPermissions, ?int $userID, $search = [], $display = []): void {
+    // $search and $display were added in 5.57: https://github.com/civicrm/civicrm-core/pull/25123
+    if (!empty($display) && isset($display['name'])) {
+      switch ($display['name']) {
+        case 'AccountContact_Synchronization_Errors_Display':
+        case 'AccountInvoice_Synchronization_Errors_Display':
+          foreach (['AccountContact', 'AccountInvoice'] as $entity) {
+            $tasks[$entity]['queue'] = [
+              'title' => E::ts('Re-queue %1', [1 => 'record']),
+              'apiBatch' => [
+                'action' => 'update',
+                'params' => ['values' => ['accounts_needs_update' => TRUE, 'error_data' => '']],
+                'runMsg' => E::ts('Queueing synchronization ...'),
+                'successMsg' => E::ts('Successfully re-queued %1 record.'),
+                'errorMsg' => E::ts('An error occurred while attempting to queue the record.'),
+              ],
+            ];
+            $tasks[$entity]['dismiss'] = [
+              'title' => E::ts('Dismiss error %1', [1 => 'record']),
+              'apiBatch' => [
+                'action' => 'update',
+                'params' => ['values' => ['is_error_resolved' => TRUE]],
+                'runMsg' => E::ts('Dismissing error ...'),
+                'successMsg' => E::ts('Successfully dismissed error for %1 record.'),
+                'errorMsg' => E::ts('An error occurred while attempting to dismiss the error.'),
+              ],
+            ];
+          }
+          break;
+
+        default:
+          return;
+      }
     }
   }
 
